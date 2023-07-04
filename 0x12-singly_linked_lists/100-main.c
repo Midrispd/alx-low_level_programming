1164c9f381790efea3c1ac90ffa67c478cf64ed9
Midrispd <midrispd41@gmail.com>
2023-07-04 12:18:07 -0700

    Write a function that prints all the elements of a list_t list

--- 0x12-singly_linked_lists/100-main.c
+++ 0x12-singly_linked_lists/100-main.c
@@ -3,9 +3,9 @@
 /**
  * main - check the code
  *
- * Return: Always 0
+ * Return: Always 0.
  */
-int main(void)
+int mai(void)
 {
 	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
 	return (0);
